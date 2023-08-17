273e7ff1627f6515af82c1228f4d7321dd74e12b
Samarthpatel2108 <141392093+Samarthpatel2108@users.noreply.github.com>
2023-08-17 14:46:48 +0530

    Add files via upload

---
+++ SAM.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import csv
+
+
+# In[4]:
+
+
+with open('C:\sqlite3\csv\hey.txt',"r",errors="ignore")as f:
+    content=f.read(30)
+    print(content)
+
+
+# In[ ]:
+
+
+
+
